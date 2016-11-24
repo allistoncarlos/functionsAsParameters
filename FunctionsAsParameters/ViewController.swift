f1f78b843c4d758eb780ed82b11264f24e6897e3
Alliston Aleixo <alliston@outlook.com>
2016-11-24 15:56:22 -0200

    Initial Commit

---
+++ FunctionsAsParameters/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunctionsAsParameters
+//
+//  Created by Alliston Aleixo on 24/11/16.
+//  Copyright © 2016 Alliston Aleixo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
